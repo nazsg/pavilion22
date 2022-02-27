ad5a969fbce5a213906d60ea993036cd1514c7fb
Nazie Sharid <post@nazs.net>
2022-02-27 12:11:36 +0000

    revamp 2

---
+++ components/appFooter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="cookie" :class="{ consent }">
+      Cookies cookies
+      <button @click="allow">agree</button>
+    </div>
+    {{ new Date().getFullYear() }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      consent: true,
+    }
+  },
+  methods: {
+    allow() {
+      this.consent = true
+      localStorage.setItem('allowCookies', true)
+    },
+  },
+  mounted() {
+    if (!localStorage.getItem('allowCookies')) this.consent = false
+  },
+}
+</script>
+
+<style>
+.consent {
+  display: none;
+}
+</style>
---
+++ components/appHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul>
+      <li><nuxt-link to="/">Home</nuxt-link></li>
+      <li><nuxt-link to="/other">Other</nuxt-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+ul {
+  display: flex;
+  justify-content: space-around;
+  list-style-type: none;
+}
+</style>
