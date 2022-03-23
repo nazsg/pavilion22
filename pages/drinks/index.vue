c242d80e9758088125a1d9d001bcb1aff1f1c195
Nazie Sharid <post@nazs.net>
2022-03-23 06:36:53 +0000

    removed duplicate charset

--- pages/drinks/index.vue
+++ pages/drinks/index.vue
@@ -47,7 +47,6 @@ export default {
   head: {
     title: 'Drinks Menu | Bangkok Pavilion',
     meta: [
-      { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
