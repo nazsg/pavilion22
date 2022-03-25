7cbb1e5e42c63499e58b6eeb2c5aff09ce224762
Nazie Sharid <post@nazs.net>
2022-03-25 22:01:46 +0000

    created new component

---
+++ components/ourClause.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="change">
+    ALL PRICES ARE SUBJECT TO CHANGE
+    <p>10% service charge will be added for parties of 6 or more</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/../sass-mixins/_styles';
+</style>
