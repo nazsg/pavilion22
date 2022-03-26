ed3967b5e562e179780253d0c788ff5d3f742370
Nazie Sharid <post@nazs.net>
2022-03-26 12:09:04 +0000

    corrected order of vue attributes

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -7,26 +7,32 @@
     <form v-else>
       <div class="group g1">
         <label for="name">Name</label>
-        <input type="text" v-model="info.name" id="name" />
+        <input 
+        id="name" />
+        v-model="info.name" 
+        type="text" 
       </div>
       <div class="group g1">
         <label for="email">Email</label>
-        <input type="text" v-model="info.email" id="email" />
+        <input 
+        id="email" />
+        v-model="info.email" 
+        type="text" 
       </div>
       <div class="group g2">
         <label for="message">Message</label>
         <textarea
           id="message"
+          v-model="info.message"
           cols="30"
           rows="10"
-          v-model="info.message"
         ></textarea>
       </div>
       <div class="antispam">
         <antispam
           v-if="result == 'unverified'"
           result="unverified"
-          v-on:updateResult="result = arguments[0]"
+          @updateResult="result = arguments[0]"
         />
       </div>
       <div class="group actions center">
@@ -35,7 +41,6 @@
           Submit
         </button>
         <button v-else class="disabled" @click.prevent>Submit</button>
-        <!-- <SendIcon :size=size /> <DeleteIcon :size=size @click="clear"/> -->
       </div>
       <div class="error">{{ formResult }}</div>
     </form>
@@ -44,22 +49,10 @@
 </template>
 
 <script>
-import SendIcon from 'vue-material-design-icons/EmailSend.vue'
-import DeleteIcon from 'vue-material-design-icons/TrashCanOutline.vue'
-import antispam from '~/components/antispam'
+// import SendIcon from 'vue-material-design-icons/EmailSend.vue'
+// import DeleteIcon from 'vue-material-design-icons/TrashCanOutline.vue'
+// import antispam from '~/components/antispam'
 export default {
-  head: {
-    title: 'Contact Us | Bangkok Pavilion',
-    meta: [
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'Contact us by sending us an email',
-      },
-    ],
-  },
-  components: { SendIcon, DeleteIcon, antispam },
   layout: 'blue',
   data() {
     return {
@@ -75,37 +68,48 @@ export default {
       formResult: '',
     }
   },
+  head: {
+    title: 'Contact Us | Bangkok Pavilion',
+    meta: [
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Contact us by sending us an email',
+      },
+    ],
+  },
   methods: {
     clear() {
-      ;(this.info.name = ''),
-        (this.info.email = ''),
-        (this.info.message = ''),
-        (this.formResult = '')
+      this.info.name = ''
+      this.info.email = ''
+      this.info.message = ''
+      this.formResult = ''
     },
     validate() {
       this.formResult = ''
-      let _ = this.info
-      let re =
-        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      const _ = this.info
+      const re =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       let err = false
-      let email_err = ''
-      if (_.name == '' || _.email == '' || _.message == '') err = true
+      let emailErr = ''
+      if (_.name === '' || _.email === '' || _.message === '') err = true
       if (_.email.trim() !== '') {
-        if (re.test(String(this.info.email).toLowerCase()) == false) {
+        if (re.test(String(this.info.email).toLowerCase()) === false) {
           err = true
-          email_err = ' and email needs a valid format'
+          emailErr = ' and email needs a valid format'
         }
       } else {
         err = true
       }
-      if (err == true) this.formResult = 'pls fill in all fields' + email_err
-      if (err == false) this.submit()
+      if (err === true) this.formResult = 'pls fill in all fields' + emailErr
+      if (err === false) this.submit()
     },
     submit() {
       this.$axios
         .post('https://bangkokpavilion.co.uk/mailer2.php', this.info)
         .then((res) => {
-          if (res.data == 'Success') {
+          if (res.data === 'Success') {
             this.formSubmitted = true
           } else {
             console.log(res.data)
